e1d6d9d26433d6a0d98a3b15d7370a874f9db0b1
gerardonastri <gerardonastri06@gmail.com>
2023-08-16 11:29:38 +0200

    analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
 import Navbar from './components/Navbar/Navbar'
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
+      <Analytics />
     </Router>
   );
 }
